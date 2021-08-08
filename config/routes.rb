37f5665c1a2f687e899c37f1013480b1adb8049b
mahoritan <papipupepon304@gmail.com>
2021-08-08 13:22:46 +0000

    [update]header,footerの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+  get 'about' => 'homes#about'
+  resources :books
+  resources :users, only: [:show, :edit, :index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
